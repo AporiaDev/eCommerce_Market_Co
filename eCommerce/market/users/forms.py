f8cb73be8bb556540b71fd9533c2b39571ef7ef7
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2024-11-23 22:58:36 -0500

    add the css

--- eCommerce/market/users/forms.py
+++ eCommerce/market/users/forms.py
@@ -29,3 +29,5 @@ class RegisterForm(forms.ModelForm):
 class LoginForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Contraseña'}))
+    
+    #
